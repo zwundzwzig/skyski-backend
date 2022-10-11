17374bd764a321b48be9d080d17c34ce7111e996
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-10-11 12:58:23 +0900

    db 테이블 수정

--- db/migrations/20221004122927_create_flights_table.sql
+++ db/migrations/20221004122927_create_flights_table.sql
@@ -7,6 +7,8 @@ CREATE TABLE flights (
     arrival_date datetime not null,
     route_id int not null,
     airline_id int not null,
+    price int not null,
+    eco varchar(100) null,
     CONSTRAINT route_id_fkey foreign key (route_id) references routes (id),
     CONSTRAINT airline_id_fkey foreign key (airline_id) references airlines (id)
 )
